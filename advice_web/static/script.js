48d4039c6c851fbaf11f98b0ee200d877e5d4015
Bruno <alencarb98@gmail.com>
2023-07-28 17:47:53 -0300

    fix load function

--- advice_web/static/script.js
+++ advice_web/static/script.js
@@ -2,25 +2,53 @@ function refreshDiv() {
     $.ajax({
         url: '/get_new_advice',
         type: 'POST',
-        success: function(data) {
+        success: function (data) {
             try {
-            // Atualize o conteúdo da div com o novo conteúdo do servidor
-            console.log(data.advice)
-            $('#advice__response').html(data.advice);
-            } catch(err) {
+                // Atualize o conteúdo da div com o novo conteúdo do servidor
+                console.log(data.advice)
+                $('#advice__response').html(data.advice['advice']);
+
+                // Atualize o valor do input com o novo advice
+                $('#btn__fav').val(data.advice);
+
+                // Atualize o atributo "action" do formulário com o novo valor do "advice"
+                var newAction = '/checkFav/' + data.advice['id'];
+                $('#favoriteForm').attr('action', newAction);
+                $('#btn__fav').submit();
+
+                // $.ajax({
+                //     url: '/',
+                //     type: 'POST',
+                //     data: { advice: data.advice['id'] },
+                //     success: function (res) {
+                //         console.log(res)
+                //     },
+                //     error: function (xhr, status, error) {
+                //         console.error(error)
+                //     }
+                // })
+            } catch (err) {
                 console.log(err)
             }
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error(error);
         }
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Adiciona o evento de clique ao ícone de fechar
-    $(".error__close, .success__close, .info__close").click(function() {
+    $(".error__close, .success__close, .info__close").click(function () {
         // Esconde o elemento .error quando o ícone de fechar é clicado
         $("#message-all").hide();
+    });
+});
+
+$(function() {
+    $(window).on("load", function() {
+        appear('"spinner__load"')
+        $(".spinner__load").fadeOut("slow");
+        hide("spinner__load")
     });
 });
